4be40e0146813d3c3e619fe2d6e50f3e21d51401
jjmoncus <jjmoncus706@gmail.com>
2025-11-07 11:46:40 +0000

    updating food with DKs and missing values to illustrate new features

--- data_raw/food.R
+++ data_raw/food.R
@@ -4,37 +4,62 @@
 library(tibble)
 
 n_respondents <- 2000
-rating_scale <- c("Very much dislike", "Somewhat dislike", "About equally like and dislike", "Somewhat like", "Very much like")
-likelihood_scale <- c("Very Low", "Low", "Moderate", "High", "Very High")
+rating_scale <- c("Very much dislike", "Somewhat dislike", "About equally like and dislike", "Somewhat like", "Very much like", "DK/Refused")
+likelihood_scale <- c("Very Low", "Low", "Moderate", "High", "Very High", "DK/Refused")
 
 # Generate synthetic data vectors
 respondent_ids <- 1:n_respondents
 
 # Age: Random integers between 18 and 80
-age_data <- sample(18:80, n_respondents, replace = TRUE)
+age_data <- sample(
+  c("18-35", "36-49", "50-64", "65+", "DK/Refused", NA_character_),
+  n_respondents,
+  replace = TRUE,
+  prob = c(0.25, 0.29, 0.21, 0.19, .03,.03)
+) %>% factor(levels = c("18-35", "36-49", "50-64", "65+"))
 
 # Gender: Sampled with different probabilities for variety (optional, but realistic)
 gender_data <- sample(
-  c("Female", "Male", "Non-binary", "Prefer not to say"),
+  c("Woman", "Man", "Other", "DK/Refused", NA_character_),
+  n_respondents,
+  replace = TRUE,
+  prob = c(0.45, 0.42, 0.05, 0.05, .03)
+) %>% factor(levels = c("Woman", "Man", "Other", "DK/Refused"))
+
+income_data <- sample(
+  c("Low income", "Middle income", "High income", "DK/Refused", NA_character_),
+  n_respondents,
+  replace = TRUE,
+  prob = c(0.35, 0.31, 0.25, 0.05, .04)
+) %>% factor(levels = c("Low income", "Middle income", "High income", "DK/Refused"))
+
+# Region: Sampled with equal probability
+regions <- c("West Coast", "East Coast", "Midwest", "South", "Mountain West", "Southwest", "DK/Refused")
+region_data <- sample(
+  c(regions, NA_character_),
+  n_respondents,
+  prob = c(.15, .15, .15, .15, .15, .15, .075, .025),
+  replace = TRUE
+  ) %>% factor(levels = regions)
+
+# party lean
+party_lean_data <- sample(
+  c("Dem/Lean Dem", "Rep/Lean Rep", "DK/Refused", NA_character_),
   n_respondents,
   replace = TRUE,
   prob = c(0.45, 0.45, 0.05, 0.05)
-) %>% factor(levels = c("Female", "Male", "Non-binary", "Prefer not to say"))
-
-# Region: Sampled with equal probability
-regions <- c("West Coast", "East Coast", "Midwest", "South", "Mountain West", "Southwest")
-region_data <- sample(regions, n_respondents, replace = TRUE) %>% factor(levels = regions)
+) %>% factor(levels = c("Dem/Lean Dem", "Rep/Lean Rep", "DK/Refused"))
 
 # food ratings
-meat_rating <- sample(rating_scale, n_respondents, replace = TRUE, prob = c(.05, .25, .25, .3, .15)) %>% factor(levels = rating_scale)
-pizza_rating <- sample(rating_scale, n_respondents, replace = TRUE, prob = c(.05, .20, .30, .35, .10)) %>% factor(levels = rating_scale)
-sushi_rating <- sample(rating_scale, n_respondents, replace = TRUE, prob = c(.10, .25, .30, .25, .10)) %>% factor(levels = rating_scale)
-vegetables_rating <- sample(rating_scale, n_respondents, replace = TRUE, prob = c(.15, .35, .25, .2, .05)) %>% factor(levels = rating_scale)
+meat_rating <- sample(c(rating_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.05, .25, .20, .34, .10, .05, .01)) %>% factor(levels = rating_scale)
+pizza_rating <- sample(c(rating_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.05, .20, .25, .34, .10, .05, .01)) %>% factor(levels = rating_scale)
+sushi_rating <- sample(c(rating_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.09, .22, .27, .26, .10, .05, .01)) %>% factor(levels = rating_scale)
+vegetables_rating <- sample(c(rating_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.15, .30, .29, .15, .05, .05, .01)) %>% factor(levels = rating_scale)
 
 # eating styles
-pesc <- sample(likelihood_scale, n_respondents, replace = TRUE, prob = c(.15, .15, .30, .2, .2)) %>% factor(levels = likelihood_scale)
-veggie <- sample(likelihood_scale, n_respondents, replace = TRUE, prob = c(.15, .25, .40, .1, .1)) %>% factor(levels = likelihood_scale)
-vegan <- sample(likelihood_scale, n_respondents, replace = TRUE, prob = c(.15, .25, .40, .15, .05)) %>% factor(levels = likelihood_scale)
+pesc <- sample(c(likelihood_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.1, .15, .30, .24, .5, .05, .01)) %>% factor(levels = likelihood_scale)
+veggie <- sample(c(likelihood_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.15, .25, .35, .14, .05, .05, .01)) %>% factor(levels = likelihood_scale)
+vegan <- sample(c(likelihood_scale, NA_character_), n_respondents, replace = TRUE, prob = c(.13, .24, .40, .14, .03, .05, .01)) %>% factor(levels = likelihood_scale)
 
 
 # Create the final tibble 'food'
@@ -42,9 +67,11 @@ food <- tibble(
   unique_id = respondent_ids %>% structure("label" = "Unique ID"),
 
   # demos
-  age = age_data %>% structure(label = "S1. How old are you?"),
+  age = age_data %>% structure(label = "AGE. age groups (recode)"),
   gender = gender_data %>% structure(label = "S2. which of the following best describes you?"),
+  income = income_data %>% structure(label = "INCOME. income (recode)"),
   region = region_data %>% structure(label = "S3. What region of the U.S. are you from?"),
+  party_lean = party_lean_data %>% structure(label = "PARTY_LEAN. party affiliation (recode)"),
 
   # food ratings
   rating_meat = pizza_rating %>% structure(label = "Q1a. Please indicate how much you like or dislike each of the following foods.:Meat"),
