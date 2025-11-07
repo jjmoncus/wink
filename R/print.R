786856226ab92c14f5029704775dcca26db6cda2
jjmoncus <jjmoncus706@gmail.com>
2025-11-07 12:55:06 +0000

    add print method

--- R/print.R
+++ R/print.R
@@ -11,3 +11,26 @@ constructor_banner <- function(data) {
       class = c("wink_banner", class(data))
     )
 }
+
+
+#' Print method for 'wink_banner' class
+#'
+#' Implements the custom print behavior for 'wink_banner' tables
+#'
+#' @param data An object of class 'wink_banner'.
+#' @param ... Additional arguments (can ignore for now)
+#'
+#' @return The input object 'data' (invisibly)
+#'
+#' @export
+#'
+#' @method print wink_banner
+print.wink_banner <- function(data, ...) {
+
+  data %>%
+    gt() %>%
+    print()
+
+  # The print method must return the object invisibly (a standard R convention)
+  invisible(data)
+}
